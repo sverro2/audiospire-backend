d506dff27a695c89251b81b2c187120ebc115f2e
Sven Brettschneider <brettschneider.sven@gmail.com>
2022-12-19 15:54:27 +0100

    adding project state

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@
 
 use rocket::{Rocket, Build};
 use rocket::serde::{Deserialize, json::Json};
+use std::collections::HashMap;
 
 #[derive(Deserialize)]
-struct Project<'r> {
-    id: &'r str
+struct Project<'a> {
+    id: &'a str
+}
+
+struct GlobalState<'a, 'b>{
+    projectStore: HashMap<&'b str, Project<'a>>
 }
 
 #[post("/", data = "<project>")]
@@ -13,8 +18,22 @@ fn backup_project(project: Json<Project>) -> &str {
     project.id
 }
 
+#[get("/<project_name>")]
+fn restore_project(project_name: &str) -> &str {
+
+    project_name
+}
+
+
+
+// #[put("/media", data = "<project>")]
+// fn backup_project(project: Json<Project>) -> &str {
+//     project.id
+// }
+
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
-        .mount("/project", routes![backup_project])
+        .mount("/project", routes![backup_project, restore_project])
+        .manage(GlobalState{projectStore: HashMap::new()})
 }
